48a96df8cab1b06d6ef6b19841bef0f5945f3890
iqrakafayat <iqrakafayat@gmail.com>
2018-11-29 09:16:28 -0800

    complete quiz implementation including radio button check uncheck and next question button

--- lectures/13/js/script.js
+++ lectures/13/js/script.js
@@ -1,4 +1,4 @@
-var questions = [{
+/**var questions = [{
     question : "When a user views a page containing a JavaScript program, which machine actually executes the script?",
     choices : [ "The User's machine running a Web browser",
         "The Web server",
@@ -28,27 +28,22 @@ displayCurrentQuestion();
 document.getElementById("quiz-message").style.display = 'none';
 function displayNext() {
 
+    currentQuestion++;
+    if(currentQuestion==questions.length)
+    {displayScore();}
+    displayCurrentQuestion();
 
-    /*Write your code here */
 }
-
 function displayCurrentQuestion() {
-    var question=document.getElementById("question");
-    var answers=document.getElementById("choice-list");
-    question.innerText=questions[currentQuestion].question;
-    answers.innerHTML=" ";
-    for(i=0; i<questions[currentQuestion].choices.length;i++)
-    {
-        answers.innerHTML+="<li" + "<input type = 'radio' name= 'Cjoices' value = ' questions[currentQuestion].choices[i]'>" + questions[currentQuestion].choices[i] + "</li>";
 
-    }
-    if(document.querySelector("input[type=radio]:checked"))
-
-    {
-
-    }
-
-    /*Write your code here */
+   var answer=document.getElementById("choice-list");
+   var q= document.getElementById("question");
+   q.innerText=questions[currentQuestion].question;
+   answer.innerHTML="";
+   for(loop = 0; loop<questions[currentQuestion].choices.length;loop++)
+       answer.innerHTML += "<li>" + "<input type=radio name = option value = questions[currentQuestion].choices[loop]" + questions[currentQuestion].choices[loop]+ "<li>";
+   if(document.querySelector("input[type=radio]:checked")==questions[currentQuestion].correctAnswer)
+       correctAnswers++;
 }
 
 function resetQuiz() {
@@ -62,4 +57,55 @@ function displayScore() {
 }
 function hideScore() {
     document.getElementById("result").style.display = 'none';
-}
+}**/
+var questions = [{
+    question : "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+    choices : [ "The User's machine running a Web browser",
+        "The Web server",
+        "A central machine deep within Netscape's corporate offices",
+        "None of the above"],
+    correctAnswer : 0
+},{
+    question : "Which of the following can't be done with client-side JavaScript?",
+    choices : [ "Validating a form",
+        "Sending a form's contents by email",
+        "Storing the form's contents to a database file on the server",
+        "None of the above"],
+    correctAnswer : 2
+},{
+    question : "Using _______ statement is how you test for a specific condition",
+    choices : [ "select",
+        "if",
+        "for",
+        "none of the above"],
+    correctAnswer : 1}];
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+displayCurrentQuestion();
+document.getElementById("quiz-message").style.display = 'none';
+function displayNext() {
+    currentQuestion++;
+    if(currentQuestion == questions.length) {
+        displayScore();}
+    displayCurrentQuestion();}
+function displayCurrentQuestion() {
+    /*Write your code here */
+    var answer = document.getElementById("choice-list");
+    //var q = document.getElementById("Question");
+    var q = document.getElementById("question");
+    q.innerText = questions[currentQuestion].question;
+    answer.innerHTML = "";
+    for(loop = 0;loop<questions[currentQuestion].choices.length;loop++)
+        answer.innerHTML += "<li>" + "<input type=radio name = option value = questions[currentQuestion].choices[loop]>" + questions[currentQuestion].choices[loop] + "</li>";
+    if(document.querySelector("input[type=radio]:checked")==questions[currentQuestion].correctAnswer)
+        correctAnswers++;}
+function resetQuiz() {
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();}
+function displayScore() {
+    document.getElementById("result").innerHTML = "you scored: " + correctAnswers + " out of: " + questions.length;
+    document.getElementById("result").style.display = 'block';}
+function hideScore() {
+    document.getElementById("result").style.display = 'none';}
